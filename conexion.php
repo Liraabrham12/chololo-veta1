f99348c2b4d4d05deaa0499b5676acb277bec974
Carlos Abraham Morando Lira <104123643+Liraabrham12@users.noreply.github.com>
2022-04-21 01:18:50 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'factura';
+
+$conection = @mysqli_connect($host,$user,$password,$db);
+
+    if(!$conection){
+        echo "Error de conecxion";
+    }
+?>
